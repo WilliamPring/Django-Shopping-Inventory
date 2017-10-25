e19b9eb3fa8e36b6d468621fd2c52e8fef5e7546
WilliamPring <mrwilliampring@gmail.com>
2017-10-24 23:38:48 -0400

    Commiting new database changes and added an admin mode

--- shopping_inventory/models.py
+++ shopping_inventory/models.py
@@ -5,6 +5,9 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=12)
+    
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Product(models.Model):
     prod_id = models.AutoField(primary_key=True)
@@ -18,7 +21,7 @@ class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
     cust_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     po_number = models.CharField(max_length=50)
-    order_date = models.CharField(max_length=50)
+    order_date = models.DateField()
 
 class Cart(models.Model):
     cust_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
