cfe1f83b3be256278c49113e2159e9fa0ef9789f
Denys <denys.politiuk@gmail.com>
2017-12-10 02:33:34 -0500

    removed empty line

--- shopping_inventory/models.py
+++ shopping_inventory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from SOAAssign4 import settings
 
 class Customer(models.Model):
     cust_id = models.AutoField(primary_key=True)
@@ -7,7 +8,6 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=12)
     
     def __str__(self):
-        
         return self.first_name + " " + self.last_name
 
 class Product(models.Model):
